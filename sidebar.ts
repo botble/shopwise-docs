028123f60df8bc6bb5d570dc9af043615b0d6080
Sang Nguyen <sangnguyenplus@gmail.com>
2023-08-16 14:57:42 +0700

    Update docs

--- sidebar.ts
+++ sidebar.ts
@@ -26,12 +26,20 @@ export default [
   {
     text: 'Usage',
     items: [
-      { text: 'Setup email', link: '/shopwise/usage-email' },
+      { text: 'Rename theme', link: '/shopwise/theme-rename' },
+      { text: 'Custom CSS/JS', link: '/shopwise/usage-custom-css-js' },
+      { text: 'Translation', link: '/shopwise/usage-translation' },
+      { text: 'Multi-language', link: '/shopwise/usage-multi-language' },
+      { text: 'Location', link: '/shopwise/usage-location' },
+      { text: 'Google Analytics', link: '/shopwise/usage-analytics' },
       { text: 'Media - Setup Amazon S3', link: '/shopwise/usage-media-s3' },
       { text: 'Media - Setup BunnyCDN', link: '/shopwise/usage-media-bunnycdn' },
       { text: 'Media - Setup Wasabi', link: '/shopwise/usage-media-wasabi' },
-      { text: 'Analytics', link: '/shopwise/usage-analytics' },
-      { text: 'Rename theme', link: '/shopwise/theme-rename' },
+      { text: 'Setup email', link: '/shopwise/usage-email' },
+      { text: 'Backup', link: '/shopwise/plugin-backup' },
+
+      { text: 'Currencies', link: '/shopwise/usage-currencies' },
+      { text: 'Setup social login', link: '/shopwise/usage-social-login' },
       { text: 'Location', link: '/shopwise/usage-location' },
       { text: 'Invoice template', link: '/shopwise/invoice-template' },
     ],
