cef80e8b1cb38facda42224bfcd1767c9b729a39
Sang Nguyen <sangnguyenplus@gmail.com>
2023-11-23 17:43:04 +0700

    Update docs

--- sidebar.ts
+++ sidebar.ts
@@ -18,25 +18,17 @@ export default [
     ],
   },
   {
-    text: 'Available plugins',
-    items: [
-      { text: 'Backup', link: '/shopwise/plugin-backup' },
-    ],
-  },
-  {
     text: 'Usage',
     items: [
       { text: 'Rename theme', link: '/shopwise/theme-rename' },
       { text: 'Custom CSS/JS', link: '/shopwise/usage-custom-css-js' },
       { text: 'Translation', link: '/shopwise/usage-translation' },
       { text: 'Multi-language', link: '/shopwise/usage-multi-language' },
-      { text: 'Location', link: '/shopwise/usage-location' },
       { text: 'Google Analytics', link: '/shopwise/usage-analytics' },
       { text: 'Media - Setup Amazon S3', link: '/shopwise/usage-media-s3' },
       { text: 'Media - Setup BunnyCDN', link: '/shopwise/usage-media-bunnycdn' },
       { text: 'Media - Setup Wasabi', link: '/shopwise/usage-media-wasabi' },
       { text: 'Setup email', link: '/shopwise/usage-email' },
-      { text: 'Backup', link: '/shopwise/plugin-backup' },
 
       { text: 'Currencies', link: '/shopwise/usage-currencies' },
       { text: 'Setup social login', link: '/shopwise/usage-social-login' },
